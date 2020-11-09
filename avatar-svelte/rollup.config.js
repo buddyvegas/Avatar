8f093c22e089d7da724d84599caba4798e4b6d57
Rémy Codron <remy.codron@axa.fr>
2020-11-09 16:29:02 +0100

    serve dev

--- avatar-svelte/rollup.config.js
+++ avatar-svelte/rollup.config.js
@@ -1,29 +1,45 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		format: 'cjs',
-		file: 'dist/index.js',
-	},
-	plugins: [
-		svelte({
-			format: 'cjs',
-			customElement: true
-		}),
-		resolve({
-			browser: true,
-			dedupe: ['svelte']
-		}),
-		commonjs(),
-		terser({
-			compress: {
-				reduce_vars: false,
-				sequences: false
-			}
-		})
-	]
+  input: "src/main.js",
+  output: {
+    format: "cjs",
+    file: production ? "build/index.js" : "public/build/index.js",
+  },
+  plugins: [
+    svelte({
+      format: "cjs",
+      customElement: true,
+    }),
+
+    resolve({
+      browser: true,
+      dedupe: ["svelte"],
+    }),
+
+    commonjs(),
+
+    !production &&
+      serve({
+        open: true,
+        contentBase: "public",
+      }),
+
+    !production && livereload(),
+
+    production &&
+      terser({
+        compress: {
+          reduce_vars: false,
+          sequences: false,
+        },
+      }),
+  ],
 };
--- avatar-svelte/src/main.js
+++ avatar-svelte/src/main.js
@@ -1,16 +1,2 @@
 import Avatar from "./Avatar.svelte";
 module.exports = Avatar;
-
-// module.exports = class {
-//   constructor({ target, props }) {
-//     this._component = new Avatar({ target, props });
-//   }
-
-//   update(props) {
-//     this._component.$set(props);
-//   }
-
-//   on(event, callback) {
-//     this._component.$on(event, callback);
-//   }
-// };
