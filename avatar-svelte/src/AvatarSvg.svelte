a4e29d7eb3f9f900f842c0d7e2b031f9f7bf915a
Rémy Codron <remy.codron@axa.fr>
2020-11-19 18:01:13 +0100

    feat: integration avatar svg

--- avatar-svelte/src/AvatarSvg.svelte
+++ avatar-svelte/src/AvatarSvg.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { getColor, getInitials } from "./utils";
 
-  export let fontColor: string = "#000000";
   export let name: string = "toto";
+  export let size: number = 60;
 
   // ajouter un fond blanc + fond transparent + texte couleur généré
 </script>
 
 <svelte:options tag="af-avatar-svg" />
-<svg version="1.1" viewBox="0 0 50 50">
+<svg version="1.1" viewBox="0 0 50 50" width={size} height={size}>
   <circle
     fill={getColor(name)}
     stroke="#fff"
@@ -17,7 +17,7 @@
     cy="25"
     r="24"
     opacity="1" />
-  <text x="17" y="30" text-anchor="middle" fill={fontColor}>
+  <text x="25" y="30" text-anchor="middle" fill="#000" opacity="0.8">
     {getInitials(name)}
   </text>
 </svg>
