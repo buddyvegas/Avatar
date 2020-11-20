185de8302b33146562a7617a9ec42ea6c25fc492
Rémy Codron <remy.codron@axa.fr>
2020-11-20 16:09:10 +0100

    feat(stencil): avatar stencil

--- avatar-stencil/src/components.d.ts
+++ avatar-stencil/src/components.d.ts
@@ -6,56 +6,36 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
+    interface AfAvatar {
+        "name": string;
+        "size": number;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
+    interface HTMLAfAvatarElement extends Components.AfAvatar, HTMLStencilElement {
     }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
+    var HTMLAfAvatarElement: {
+        prototype: HTMLAfAvatarElement;
+        new (): HTMLAfAvatarElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
+        "af-avatar": HTMLAfAvatarElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
+    interface AfAvatar {
+        "name"?: string;
+        "size"?: number;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
+        "af-avatar": AfAvatar;
     }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
+            "af-avatar": LocalJSX.AfAvatar & JSXBase.HTMLAttributes<HTMLAfAvatarElement>;
         }
     }
 }
